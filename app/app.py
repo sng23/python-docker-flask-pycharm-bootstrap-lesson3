b83e5719c2fd81481bc107e0f3c6e8a1354f58c2
sng23 <sng23@njit.edu>
2020-05-04 18:43:38 -0400

    Feature: View page.

--- app/app.py
+++ app/app.py
@@ -25,12 +25,12 @@ def index():
     return render_template('index.html', title='Home', user=user, hw=result)
 
 
-@app.route('/view/<int:city_id>', methods=['GET'])
-def record_view(city_id):
+@app.route('/view/<int:hw_id>', methods=['GET'])
+def record_view(hw_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM tblHwImport WHERE id=%s', hw_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', city=result[0])
+    return render_template('view.html', title='View Form', hw=result[0])
 
 
 @app.route('/edit/<int:city_id>', methods=['GET'])
