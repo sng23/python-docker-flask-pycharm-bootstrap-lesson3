27dbaf04d8ecd0bceac9054a2aee0cd014ba642c
sng23 <sng23@njit.edu>
2020-05-04 18:53:01 -0400

    Feature: Edit page save.

--- app/app.py
+++ app/app.py
@@ -41,14 +41,11 @@ def form_edit_get(hw_id):
     return render_template('edit.html', title='Edit Form', hw=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<int:hw_id>', methods=['POST'])
+def form_update_post(hw_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
-    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
-    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    inputData = (request.form.get('fldHeightInches'), request.form.get('fldWeightPounds'), hw_id)
+    sql_update_query = """UPDATE tblHwImport t SET t.fldHeightInches = %s, t.fldWeightPounds = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
